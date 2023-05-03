e2fa1409340b0cdd26349b232c9b48408fa8eab4
LHKong7 <jamesklh5666@gmail.com>
2023-05-03 18:52:25 +0800

    feat: update current progress

---
+++ HTML/index.js
@@ -0,0 +1,14 @@
+const Webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const WebpackConfig = require ('./webpack.config.js');
+
+const compiler = Webpack(WebpackConfig);
+const devServerOptions = { ...WebpackConfig.devServer, open: true };
+const server = new WebpackDevServer(devServerOptions, compiler);
+
+const runServer = async () => {
+  console.log('Starting server...');
+  await server.start();
+};
+
+runServer();
--- HTML/simpleServer.js
+++ HTML/simpleServer.js
@@ -1,19 +1,11 @@
-const http = require("http");
-const { readFile } = require('node:fs/promises');
+const express = require('express');
+const app = express();
 
 const host = 'localhost';
 const port = 8000;
 
-const requestListener = function (req, res) {
-    readFile(__dirname + "/intersectionObserver.html", { encoding: 'utf8' })
-    .then((content) => {
-        res.setHeader("Content-Type", "text/html");
-        res.writeHead(200);
-        res.end(content);
-    });
-};
+app.use(express.static('public'));
 
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
